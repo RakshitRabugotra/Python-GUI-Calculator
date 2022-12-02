eb673a54f55885f50119b7c1dcd9732fe56eff62
Rakshit Rabugotra <90450441+RakshitRabugotra@users.noreply.github.com>
2022-12-02 19:28:06 +0530

    Added private method to Calculator class '__layout' declaraing and packing Tkinter widgets.\nAdded constants for each widget's properties and pack/grid/place method properties

--- app.py
+++ app.py
@@ -5,10 +5,16 @@ class Calculator:
     def __init__(self, master: tk.Tk = None):
         """
         """
+        # If the master is None, then initialize a new master
+        master = tk.Tk() if (master is None) else master
+
         # Configure the master instance
         master.config(bg=ct.MASTER['bg'])
         master.title(ct.TITLE)
         master.resizable(*ct.RESIZABLE)
+
+        # Making a few TK variables, to use with TK widgets
+        self.tkStringVarEntryBox = tk.StringVar(value="0")
 
         # Set the master as class property
         self.master = master
@@ -24,12 +30,30 @@ class Calculator:
             This function runs the Tkinter instance
             by calling masters' .mainloop() method
         """
+        self.__layout()
         self.master.mainloop()
 
+    # This is a private function
+    def __layout(self):
+        """
+            This function contains the layout of all the widgets. Also,
+            it is called only once when invoking .run() method of this class
+        """
+        # Copying the value to new variable, so we don't have to write (self)
+        # multiple times
+        master = self.master
+
+        # NOTE: We will use CamelCase to name Tkinter Widgets, as the names tend to
+        #       get too long to write in snake_case.
+
+        # Create an Entry widget to display the result
+        tkEntryBox = tk.Entry(master, textvariable=self.tkStringVarEntryBox, **ct.ENTRY)
+        tkEntryBox.pack(side=tk.TOP, anchor=tk.N, fill=tk.X, **ct.ENTRY_PACK)
 
 # This statement will be true if the user is directly running
 # this python file
 if __name__ == '__main__':
+
     # You can create an instance of the Calculator
     app = Calculator()
     app.run()
--- src/constants.py
+++ src/constants.py
@@ -9,3 +9,27 @@ RESIZABLE = (False, False)
 """
     Tkinter widget properties
 """
+# Properties of the master Tkinter widget
+MASTER = {
+    'bg': "#000",
+
+}
+
+# NOTE: The Widgets are declaration is arranged in alphabetical order
+
+# Properties of any Entry Widget
+ENTRY = {
+    'font': "Roboto",
+    'bg': "#fff",
+    'fg': "#001",
+    'width': 50,
+    'justify': 'right',
+}
+
+# NOTE: The pack, grid and place method arguments are here for each widget
+ENTRY_PACK = {
+    'ipadx': 5,
+    'ipady': 5,
+    'padx': 10,
+    'pady': 10,
+}
